12b754ff79a5bfe311924d9325e3eee398943fd4
Maryna Ilinova <80976268+Maryinad@users.noreply.github.com>
2023-01-09 09:32:42 +0200

    Added 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+refs = {
+  btnStart: document.querySelector('.js-btn-start'),
+  btnStop: document.querySelector('.js-btn-stop'),
+  body: document.querySelector('body'),
+};
 
+let timerID = null;
+
+refs.btnStart.addEventListener('click', changeColor);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeColor() {
+  refs.btnStart.disabled = true;
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+refs.btnStop.addEventListener('click', () => {
+  refs.btnStart.disabled = false;
+  clearInterval(timerId);
+});
